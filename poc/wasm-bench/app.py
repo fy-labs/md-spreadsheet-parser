2326ba63209cf197caacd19f5447945ca1fcd869
f-y <f-y@users.noreply.github.com>
2026-01-05 14:14:38 +0900

    chore: Remove accidentally committed poc directory

--- poc/wasm-bench/app.py
+++
@@ -1,8 +0,0 @@
-import md_spreadsheet_parser.parsing
-from spreadsheet_parser import exports
-
-
-class SpreadsheetParser(exports.SpreadsheetParser):
-    def parse(self, content: str) -> int:
-        workbook = md_spreadsheet_parser.parsing.parse_workbook(content)
-        return len(workbook.sheets)
